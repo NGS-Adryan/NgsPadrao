8750dd759bdb2d6d9d895f235d9dd86ea4b2043e
NGS-Adryan <ngs.adryan@gmail.com>
2024-01-25 14:08:12 -0300

    criando a main
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from logger import HOUR_START_PROGRAM, log_and_print
+
+def main() -> None:
+    print(f'Iniciou as {HOUR_START_PROGRAM}')
+    pass
+
+
+if __name__ == '__main__':
+    main()
