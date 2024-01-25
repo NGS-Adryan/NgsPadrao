d774563df2e38da2f75aafd9519ecb31dc79dbaa
NGS-Adryan <ngs.adryan@gmail.com>
2024-01-25 14:06:08 -0300

    adicionando arquivo de cryptography
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ crypto.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from cryptography.fernet import Fernet
+
+def decrypt_password(encrypted_password: bytes) -> str:
+    """Função para descriptografar a senha do banco
+
+    Args:
+        encrypted_password (bytes): Passando a senha encriptografada
+
+    Returns:
+        str: Senha do banco
+    """
+    KEY = open('token.key', 'rb').read()
+    return Fernet(KEY).decrypt(encrypted_password).decode()
+    
+
+if __name__ == '__main__':
+    print()
