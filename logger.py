507f6bf6ced9c07567d96e951008d07080315afc
NGS-Adryan <ngs.adryan@gmail.com>
2024-01-25 14:06:47 -0300

    adicionando arquivo de logger
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ logger.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from datetime import datetime
+
+from ngsconfig import DIR_LOG, NAME_PROJECT
+
+START_PROGRAM = datetime.now()
+DATE_START_PROGRAM = START_PROGRAM.strftime('%Y-%m-%d')
+HOUR_START_PROGRAM = START_PROGRAM.strftime('%H:%M:%S')
+
+logging.basicConfig(
+    filename=f'{DIR_LOG}{NAME_PROJECT}_{DATE_START_PROGRAM}.log',
+    filemode='a',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    datefmt='%d-%b-%y %H:%M:%S'
+)
+
+def log_and_print(msg: str, level: int=logging.INFO) -> None:
+    """Escreve no arquivo de logs e printar no terminal
+
+    Args:
+        msg (str): Menssagem a ser inserida no log
+        level (int): Nível do log, por padrão é logging.INFO.
+
+    Returns:
+        None: Não tem retorno
+    """
+    print(msg)
+    match level:
+        case logging.INFO:
+            return logging.info(msg)
+        case logging.CRITICAL:
+            return logging.critical(msg)
+        case logging.ERROR:
+            return logging.error(msg)
+        case logging.WARNING:
+            return logging.warning(msg)
+        case logging.DEBUG:
+            return logging.debug(msg)
+
+
+if __name__ == '__main__':
+    log_and_print('teste')
