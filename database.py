060bc4be76486a6f9ac267a2918ab82743a98027
NGS-Adryan <ngs.adryan@gmail.com>
2024-01-25 14:06:26 -0300

    adicionando arquivo de conexão ao banco
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ database.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from oracledb import init_oracle_client, makedsn, connect
+
+from ngsconfig import CLIENT, HOST, PORT, SERVICE, ORACLE_USER, ORACLE_PASS
+from dbquerys import *
+
+init_oracle_client(lib_dir=CLIENT)
+DSN = makedsn(host=HOST, port=PORT, service_name=SERVICE)
+
+def test_connection() -> bool:
+    """Função para testar se está funcionando o banco
+
+    Returns:
+        bool: Se está ou não funcionando
+    """
+    with connect(DSN, user=ORACLE_USER, password=ORACLE_PASS) as CONNECTION:
+        with CONNECTION.cursor() as CURSOR:
+            CURSOR.execute(QUERY_TEST)
+            return True if CURSOR.fetchall() else False
+
+
+if __name__ == '__main__':
+    print(test_connection())
