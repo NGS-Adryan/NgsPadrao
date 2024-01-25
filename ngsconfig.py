728b10131a69165f0c98c7edf1698634f80c6bcd
NGS-Adryan <ngs.adryan@gmail.com>
2024-01-25 14:08:00 -0300

    adicionando arquivo de configuração no banco
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

---
+++ ngsconfig.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from configparser import ConfigParser
+from crypto import decrypt_password
+
+NAME_PROJECT = 'NgsPadrao'
+
+CONFIG = ConfigParser()
+CONFIG.read(f'{NAME_PROJECT}.ini')
+
+NGS_CONFIG = CONFIG['config']
+EMP, DIR_LOG = NGS_CONFIG['emp'], NGS_CONFIG['diretorio_log']
+
+DB = CONFIG['database']
+ORACLE_USER, PASS_DB, CLIENT = DB['user'], DB['password'], DB['client_dir']
+HOST, PORT, SERVICE = DB['host'], int(DB['port']), DB['service']
+
+ORACLE_PASS = decrypt_password(encrypted_password=PASS_DB.encode())
+
+if __name__ == '__main__':
+    print(ORACLE_PASS)
