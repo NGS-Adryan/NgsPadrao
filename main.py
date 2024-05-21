c05361708190d66983017e30723681890082b776
NGS-Adryan <ngs.adryan@gmail.com>
2024-05-21 10:56:01 -0300

    refatorando arquivo main
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# -*- coding: utf-8 -*-
-
 """
    =|=-*-=-*-=-*-=-*-=*-=-*-=-*-=|=
     | Created by Adryan Maikel!  |
     | Date: 26/01/2024           |
+    | Versão: 1.0                |
    =|=-*-=-*-=-*-=-*-=*-=-*-=-*-=|=
 
 """
 
-from logger import HOUR_START_PROGRAM# , log_and_print
+from ngsconfig import PROJECT_NAME as pname, log_and_print as lprint
 
-def main() -> None:
-    print(f'Iniciou as {HOUR_START_PROGRAM}')
-    # log_and_print()
-    pass
+__version__ = "1.0"
+
+
+def main():
+    lprint(f"{pname} iniciado. Versão: {__version__}")
 
 
 if __name__ == '__main__':
